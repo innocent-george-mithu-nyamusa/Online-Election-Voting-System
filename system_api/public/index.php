0aa7b76e5b7e98a79ec5d927ece2a09a1c650e19
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-01 14:56:37 +0200

    checking fix

--- system_api/public/index.php
+++ system_api/public/index.php
@@ -5,6 +5,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require ($_SERVER["DOCUMENT_ROOT"].'/voters/system_api/vendor/autoload.php');
 
 $app = new \Slim\App;
+
 $app->get('/hello/{name}', function (Request $request, Response $response){
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
