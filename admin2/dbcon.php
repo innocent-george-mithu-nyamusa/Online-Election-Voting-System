66d26d2eedc0044bc0cb60268db29dd4e0a2aec7
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-01 07:30:37 +0200

    fixed stuff

--- admin2/dbcon.php
+++ admin2/dbcon.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'vote');
+	$conn = new mysqli('localhost', 'u271646766_voters', 'Coollifestyle@1', 'u271646766_voters');
 	
 	if(!$conn){
 		die("Error: Failed to connect to database");
--- register/footer.php
+++ register/footer.php
@@ -1,12 +1,8 @@
 
-<footer class="footer">
+<footer class="footer center">
 	<hr/>
-&copy2021. Developed by Vishal Singla, Summer Gagneja, Ujjwal Khanna.
+&copy 2022.
 <br/>
 Online Voting System.
-<br/>
-Minor Project 2021. Guru Nanak Dev Engineering College, Ludhiana
-<br/>
-Contact: <a href="tel:9876543210">9876543210</a> | Email: <a href="mailto:samar.gagneja@gmail.com">samar.gagneja@gmail.com</a>
-
 </footer>
+
