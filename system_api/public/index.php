826dead5909031151fca2fe967561f94daf31f1c
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-01 10:43:19 +0200

    update1

--- system_api/public/index.php
+++ system_api/public/index.php
@@ -3,7 +3,6 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
-require '../src/config/db.php';
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response){
