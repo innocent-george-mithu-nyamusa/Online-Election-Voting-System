aa5b92254b608f96b1c7e20e38c563ac2813eb8f
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-01 16:16:41 +0200

    updated factory

--- system_api/public/index.php
+++ system_api/public/index.php
@@ -2,11 +2,11 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-
+use Slim\Factory\AppFactory as AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = Slim\Factory\AppFactory::create();
+$app = AppFactory::create();
 
 $app->get('/hello/{name}', function (Request $request, Response $response){
     $name = $request->getAttribute('name');
