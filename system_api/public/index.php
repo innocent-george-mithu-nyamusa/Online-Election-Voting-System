a3ab938de3798d9fb775d78119fac3fc2c1f0507
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-01 11:01:23 +0200

    update

--- system_api/public/index.php
+++ system_api/public/index.php
@@ -2,7 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require __DIR__.'/votersvendor/autoload.php';
+require __DIR__.'/voters/vendor/autoload.php';
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response){
