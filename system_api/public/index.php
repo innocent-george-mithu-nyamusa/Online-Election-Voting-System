1d56dab5a88b69cc7d9c4a61a10bcfd055a994d6
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-02 08:52:23 +0200

    fix

--- system_api/public/index.php
+++ system_api/public/index.php
@@ -2,7 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require __DIR__.'../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 $app = new \Slim\App;
 
