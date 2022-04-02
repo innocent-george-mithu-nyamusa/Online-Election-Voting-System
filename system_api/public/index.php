dc565de4dc49f4b37a17091fe562906dc3e1a6b3
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-02 19:03:28 +0200

    added method call

--- system_api/public/index.php
+++ system_api/public/index.php
@@ -6,16 +6,31 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = new \Slim\App;
 
+$fingeprint;
+
+function getFingerprint(){
+    global $fingeprint;
+    return $fingeprint;
+}
+
+function setFingerprint(string $rfingerId) {
+    global $fingeprint;
+
+    $fingeprint = $rfingerId;
+}
+
 $app->get('/api/{id}', function (Request $request, Response $response){
     $finger = $request->getAttribute('id');
-    $response->getBody()->write("Hello, $finger");
-    print_r($finger);
+    // $response->getBody()->write("Hello, $finger");
+
+    setFingerprint($finger);
     
     return $response;
+
 });
  
-
 //Students Routes
 // require '../src/routes/students.php';
 $app->run();
+
 ?>
