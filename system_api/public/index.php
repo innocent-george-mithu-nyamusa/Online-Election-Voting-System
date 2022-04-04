fea0a4a9823ca0f85f1cfc1e80a3262dd056d661
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-04 20:28:25 +0200

    messages

--- system_api/public/index.php
+++ system_api/public/index.php
@@ -13,7 +13,7 @@ $app->get('/api/{id}', function (Request $request, Response $response) {
     global $pdo;
     $finger = $request->getAttribute('id');
 
-    // $response->getBody()->write("Hello, $finger");
+    $response->getBody()->write("id is:, $finger");
 
     setFingerprint($finger);
     $insertStmt = $pdo->prepare("INSERT INTO temp(temp_id) VALUES ($finger)");
