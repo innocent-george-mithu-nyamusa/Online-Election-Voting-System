7413237d5c2b7bc4c78eeec2cda61d8ef6560721
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-05 12:24:11 +0200

    fix

--- register/index.php
+++ register/index.php
@@ -83,21 +83,19 @@ include("dbcon.php");
 								</select>
 							</div>
 							<br />
-
 							<script>
 								setInterval(function() {
 
-									document.getElementById("id_number").value = <?php echo getRecentId(); ?>
-
 									<?php
+
 									if (getRecentId() != '') {
 										echo  '<center><button name = "save" type="submit" >Proceed Next</button></center>';
 									} else {
 										echo '<center><button disabled="true" >Waiting to</button></center>';
 									}
-
 									?>
-								}, 10000)
+
+								}, 1000);
 							</script>
 
 							<!-- <center><button name = "save" type="submit">Proceed Next</button></center> -->
