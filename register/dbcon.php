ba467eaa283217bfa998a2e33927219aa0104a53
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-05 11:44:55 +0200

    fix

--- register/dbcon.php
+++ register/dbcon.php
@@ -1,7 +1,6 @@
 <?php
-	$conn = new mysqli('localhost', 'u271646766_voters', 'Coollifestyle@1', 'u271646766_voters');
-	
-	if(!$conn){
-		die("Error: Failed to connect to database");
-	}
-?>	
+$conn = new mysqli('localhost', 'u271646766_voters', 'Coollifestyle@1', 'u271646766_voters');
+
+if (!$conn) {
+	die("Error: Failed to connect to database");
+}
