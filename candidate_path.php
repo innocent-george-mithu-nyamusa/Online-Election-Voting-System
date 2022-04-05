13cc0150a5c5404c1f44fd679f92c562e1ad62a0
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-05 14:00:01 +0200

    heck

--- candidate_path.php
+++ candidate_path.php
@@ -1,42 +1,60 @@
+<?php
 
-<?php include ('head.php');?>
+include('head.php');
+include('system_api/src/config/db.php');
+
+$db = new db();
+$pdo = $db->connect();
+
+
+?>
 
 <body>
 
     <div id="wrapper">
 
-      
-              <?php include ('view_banner.php');?>
-       
+
+        <?php include('view_banner.php'); ?>
+
         <div id="page-wrapper">
 
-    <heading class="menue-select">
-            <center>
-                <select onchange = "page(this.value)">
-                <option disabled selected>Select Candidate Group</option>
-                <option value = "candidates/pm.php">Prime Minister</option>
-                <option value = "candidates/cm.php">Chief Minister</option>
-                <option value = "candidates/mla.php">MLA</option>
-                </select>
-            </center>
+            <heading class="menue-select">
+                <center>
+                    <select onchange="page(this.value)">
+                        <option disabled selected>Select Candidate Group</option>
+                        <?php
+                        $stmt = $pdo->prepare("SELECT * FROM candidate");
+                        $stmt->execute();
+                        $allValues = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    </heading> 
+                        foreach ($allValues as $value) {
+                        ?>
+                            <option value="candidates/pm.php"><?php echo $value["position"] ?></option>
+                        <?php
+                        }
+                        ?>
+                        <!-- <option value="candidates/pm.php">Prime Minister</option>
+                        <option value="candidates/cm.php">Chief Minister</option>
+                        <option value="candidates/mla.php">MLA</option> -->
+                    </select>
+                </center>
+
+            </heading>
         </div>
         <!-- /#page-wrapper -->
 
     </div>
     <!-- /#wrapper -->
-    <?php    
-        include ('footer.php');
-        ?>
+    <?php
+    include('footer.php');
+    ?>
 
     <script>
-    function page (src){
-        window.location=src;
-    }
+        function page(src) {
+            window.location = src;
+        }
     </script>
 
 </body>
 
 </html>
-
