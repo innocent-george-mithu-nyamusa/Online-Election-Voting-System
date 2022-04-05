35157865fca4df137f6bdcf159dcbdd7a1259b12
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-05 15:32:00 +0200

    fix

--- register/getfingerprint.php
+++ register/getfingerprint.php
@@ -6,8 +6,11 @@ include(__DIR__ . "/../system_api/public/file.php");
 $stmt = $pdo->prepare("SELECT temp_id FROM temp");
 $allResults = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
+echo $allResults["temp_id"][0];
+
 if (isset($allResults["temp_id"][0])) {
     echo $allResults["temp_id"][0];
 } else {
-    echo "null";
+    // echo "null";
 }
