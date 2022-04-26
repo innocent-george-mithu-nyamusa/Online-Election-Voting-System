76dadd748cfc8bba27144a791908432e8de54ceb
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-04-26 09:42:59 +0200

    added candidate path

--- candidates/view_banner.php
+++ candidates/view_banner.php
@@ -8,7 +8,7 @@
             <li>
                 <a href="../index.php">Home</a>
             </li>
-            <li><a href="../view.php">Candidates</a></li>
+            <li><a href="../candidate_path.php">Candidates</a></li>
 
             <li><a href="../register/index.php">Register</a></li>
             <li><a href="../voters.php">Voter List</a></li>
