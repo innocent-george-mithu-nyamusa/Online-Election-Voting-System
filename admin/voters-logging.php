2fe291e6ef4e88687a18423960f3797c3e129e5c
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-05-27 22:36:36 +0200

    upadted votes logging

--- admin/voters-logging.php
+++ admin/voters-logging.php
@@ -11,7 +11,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h3 class="page-header">Voters Logging</h3>
+                    <h3 class="page-header">Votes Logging</h3>
                
 				<hr/>
                     <div class="panel panel-default">
