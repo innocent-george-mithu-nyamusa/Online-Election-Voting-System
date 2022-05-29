fb1eac138068d6f5c9e8718b6213de0236dbfc08
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-05-29 23:43:19 +0200

    checked voted logic

--- login_query.php
+++ login_query.php
@@ -6,7 +6,7 @@ if (isset($_POST['login'])) {
 	$idno = $_POST['idno'];
 	$password = $_POST['password'];
 
-	$result = $conn->query("SELECT * FROM voters WHERE id_number = '$idno' && password = '" . md5($password) . "' && `account` = 'Active' && `status` = 'Unvoted'") or die(mysqli_errno($conn));
+	$result = $conn->query("SELECT * FROM voters WHERE id_number = '$idno' && password = '" . md5($password) . "' && `account` = 'Active'") or die(mysqli_errno($conn));
 	$row = $result->fetch_array();
 
 	$vote_check = $conn->query("SELECT * FROM `voters` WHERE id_number = '$idno' && password = '" . md5($password) . "' && `status` = 'Voted' && `president`= '1' && `secretary`='1' && `secretary_general`='1' && `chairman`='1' && `finance_dierctor`=='1'");
