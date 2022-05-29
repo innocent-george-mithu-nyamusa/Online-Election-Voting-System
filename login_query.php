b09caa45afa71a2925cadd769e79cbf4d67c5e2e
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-05-29 20:49:18 +0200

    added new values

--- login_query.php
+++ login_query.php
@@ -7,6 +7,7 @@ if (isset($_POST['login'])) {
 
 	$result = $conn->query("SELECT * FROM voters WHERE id_number = '$idno' && password = '" . md5($password) . "' && `account` = 'active' && `status` = 'Unvoted'") or die(mysqli_errno($conn));
 	$row = $result->fetch_array();
+
 	$voted = $conn->query("SELECT * FROM `voters` WHERE id_number = '$idno' && password = '" . md5($password) . "' && `status` = 'Voted' && `president`= '1' && `secretary`='1' && `secretary_general`='1' && `chairman`='1' && `finance_dierctor`=='1'")->num_rows;
 	$numberOfRows = $result->num_rows;
 
@@ -15,9 +16,9 @@ if (isset($_POST['login'])) {
 
 		$_SESSION['voters_id'] = $row['voters_id'];
 		$_SESSION['reg_number'] =  $_POST['reg_number'];
+
 		header('location:vote1.php');
 	}
-
 
 	if ($voted == 1) {
 ?>
@@ -28,7 +29,7 @@ if (isset($_POST['login'])) {
 	} else {
 	?>
 		<script type="text/javascript">
-			alert('Your account is not Actived')
+			alert('Your account is not Activated')
 		</script>
 <?php
 	}
