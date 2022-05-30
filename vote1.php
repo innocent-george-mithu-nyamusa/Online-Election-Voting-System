2fa0827e5c2f5368c808310c6efc56238f094de1
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2022-05-30 18:02:41 +0200

    added checks

--- vote1.php
+++ vote1.php
@@ -16,31 +16,31 @@ session_start();
                     <select onchange="page(this.value)">
                         <option disabled selected>Select Candidate Group</option>
 
-                        <?php if (!($_SESSION["pm_id"])) { ?>
+                        <?php if (!isset($_SESSION["pm_id"])) { ?>
                             <option value="pm_vote.php" name="pm">
                                 President
                             </option>
                         <?php } ?>
 
-                        <?php if (!($_SESSION["finance_id"])) { ?>
+                        <?php if (!isset($_SESSION["finance_id"])) { ?>
                             <option value="finance.php" name="pm">
                                 Finance Director
                             </option>
                         <?php } ?>
 
-                        <?php if (!($_SESSION["ch_id"])) { ?>
+                        <?php if (!isset($_SESSION["ch_id"])) { ?>
                             <option value="chairman.php" name="pm">
                                 Chairman
                             </option>
                         <?php } ?>
 
-                        <?php if (!($_SESSION["cm_id"])) { ?>
+                        <?php if (!isset($_SESSION["cm_id"])) { ?>
                             <option value="cm_vote.php">
                                 Secretary General
                             </option>
                         <?php } ?>
 
-                        <?php if (!($_SESSION["mla_id"])) { ?>
+                        <?php if (!isset($_SESSION["mla_id"])) { ?>
                             <option value="mla_vote.php">
                                 Secretary
                             </option>
