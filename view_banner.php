b48e77cb599015fd7c54f336f2c0ad31cc1c1979
SRC2022 <104890246+SRC2022@users.noreply.github.com>
2022-06-06 14:39:15 +0200

    Update view_banner.php

--- view_banner.php
+++ view_banner.php
@@ -15,7 +15,7 @@
 </style>
  
 <div class="text-center text-success animateuse"   style="background-color: #40AFF8; font-size:25px;text-align:center; margin-top:-10px; padding-top:20px;">
-      <img src ="img/cutt.png" width="70"> <a style="color:white; font-weight:bold; font-size:30px;">   CUT SRC Fingerprint Voting System  </a>  <img src ="img/cutt.png" width="70">
+      <img src ="img/cutt.png" width="90"> <a style="color:white; font-weight:bold; font-size:30px;">   CUT SRC Fingerprint Voting System  </a>  <img src ="img/cutt.png" width="90">
 
     <nav class="nav-menue">
         <ul>
