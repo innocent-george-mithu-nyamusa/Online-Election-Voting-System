317077b4744c691e76605134c73ce19b03138c4d
SRC2022 <104890246+SRC2022@users.noreply.github.com>
2022-06-06 15:40:06 +0200

    Update slider.php

--- slider.php
+++ slider.php
@@ -10,8 +10,8 @@
 <div id="slider">
 	<figure>
 		<img src="img/voting1.png">
-		<img src="img/voting4.jpg">
-		<img src="img/voting3.jpg">
+		//<img src="img/voting4.jpg">
+		<img src="img/set.jpg">
 		<img src="img/voting2.jpg">
     </figure>
 </div>
